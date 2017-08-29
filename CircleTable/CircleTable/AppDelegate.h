e2eea15840947c92094e583de76a3ffa95fff54b
Emiaostein <cszwdy@gmail.com>
2017-08-29 15:33:56 +0800

    Commit init.

---
+++ CircleTable/CircleTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CircleTable
+//
+//  Created by Chen on 29/08/2017.
+//  Copyright © 2017 HUAYUN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CircleTable/CircleTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CircleTable
+//
+//  Created by Chen on 29/08/2017.
+//  Copyright © 2017 HUAYUN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
